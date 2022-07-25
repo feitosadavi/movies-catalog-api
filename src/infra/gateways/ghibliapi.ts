46d26f8f666c972117dc44df2ca6064694dea0c0
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-25 18:26:38 -0300

    refactor(Ghibliapi)!: corectly maping movies and using correct interface

--- src/infra/gateways/ghibliapi.ts
+++ src/infra/gateways/ghibliapi.ts
@@ -1,15 +1,15 @@
 import { GhibliapiGateway } from '@/application/gateways';
-import { MovieProps, ProductWithoutId } from '@/domain/entities';
+import { MovieProps } from '@/domain/entities';
 import { HttpGetClient } from './protocols';
 
-export type GhibliapiMovie = Omit<ProductWithoutId, 'banner'> & { movie_banner: string }
+export type GhibliapiMovie = Omit<MovieProps, 'banner'> & { movie_banner: string }
 
 export class Ghibliapi implements GhibliapiGateway {
   constructor(private readonly httpClient: HttpGetClient) { }
 
   async loadMovies (): Promise<MovieProps[]> {
     const ghibliapiMovies = await this.httpClient.get<GhibliapiMovie[]>({ url: 'https://ghibliapi.herokuapp.com/films' })
-    const movies = ghibliapiMovies.map(apiMovie => ({ ...apiMovie, banner: apiMovie.movie_banner }))
+    const movies = ghibliapiMovies.map(({ movie_banner, ...apiMovie }) => ({ ...apiMovie, banner: movie_banner }))
     return movies
   }
 }
