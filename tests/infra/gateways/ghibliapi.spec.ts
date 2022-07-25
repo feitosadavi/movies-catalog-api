6d5224b9d0603eb5fe0c8830eb4d56a801cd8c92
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-25 18:27:26 -0300

    refactor!: using correct fakes

--- tests/infra/gateways/ghibliapi.spec.ts
+++ tests/infra/gateways/ghibliapi.spec.ts
@@ -1,5 +1,6 @@
 import { mockHttpGetClient } from '../mocks/client'
 import { HttpGetClient, Ghibliapi, GhibliapiMovie } from '@/infra/gateways'
+import { makeFakeMovies } from '@tests/domain/fakes'
 
 type SutType = {
   httpGetClientStub: HttpGetClient
@@ -7,13 +8,19 @@ type SutType = {
 }
 
 describe('Ghibliapi', () => {
-  const makeFakeGhibliapiMovie = (): GhibliapiMovie => ({
+  const makeFakeGhibliapiMovie = (): GhibliapiMovie[] => ([{
     title: 'any_title',
     description: 'any_description',
     movie_banner: 'any_banner',
     producer: 'any_producer',
     director: 'any_director'
-  })
+  }, {
+      title: 'other_title',
+      description: 'other_description',
+      movie_banner: 'other_banner',
+      producer: 'other_producer',
+      director: 'other_director'
+    }])
 
   const makeSut = (): SutType => {
     const httpGetClientStub = mockHttpGetClient(makeFakeGhibliapiMovie())
@@ -35,8 +42,7 @@ describe('Ghibliapi', () => {
     it('should return correct data', async () => {
       const { sut } = makeSut()
       const movies = await sut.loadMovies()
-      const { movie_banner, ...fakeGhibliapiMovie } = makeFakeGhibliapiMovie()
-      expect(movies).toEqual({ ...fakeGhibliapiMovie, banner: makeFakeGhibliapiMovie().movie_banner })
+      expect(movies).toEqual(makeFakeMovies())
     })
   })
 })
