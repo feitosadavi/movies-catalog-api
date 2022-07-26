2f0c713ecd54b6128ae7fbb11fb1c0db66d53eec
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-25 22:02:52 -0300

    feat: create /get-movies route

--- src/main/routes/movie.routes.ts
+++ src/main/routes/movie.routes.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
 import { MovieController } from '@/application/controllers/movie.controller'
-import { SaveMovies } from '@/application/usecases'
+import { GetMovies, SaveMovies } from '@/application/usecases'
 import { AxiosHttpClient, Ghibliapi } from '@/infra/gateways'
 import { MoviesMongoRepository } from '@/infra/repos/mongo'
+import { GetMoviesController } from '@/application/controllers/get-movies.controller'
+import { adaptRoute } from '../adapters'
 
 const makeMovieController = () => {
   const axiosHttpClient = new AxiosHttpClient()
@@ -14,9 +16,17 @@ const makeMovieController = () => {
   return new MovieController(saveMovies)
 }
 
+const makeGetMoviesController = () => {
+  const moviesRepository = new MoviesMongoRepository()
+  const getMovies = new GetMovies(moviesRepository)
+  return new GetMoviesController(getMovies)
+}
+
 export default (router: Router): void => {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.get('/save-movies', (req, res) => {
     makeMovieController().handle(req)
   })
+
+  router.get('/get-movies/:skip?/:limit?', adaptRoute(makeGetMoviesController()))
 }
