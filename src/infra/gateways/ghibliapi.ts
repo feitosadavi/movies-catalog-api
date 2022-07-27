ad09626fa5af01cdb576d3f604131360c6cd8249
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-26 23:49:59 -0300

    feat(Ghibliapi)!: ensure mapMovie maps all movie inserted, transforming movie_banner into banner

--- src/infra/gateways/ghibliapi.ts
+++ src/infra/gateways/ghibliapi.ts
@@ -7,17 +7,20 @@ export type GhibliapiMovie = Omit<MovieProps, 'banner'> & { movie_banner: string
 export class Ghibliapi implements GhibliapiGateway {
   constructor(private readonly httpClient: HttpGetClient) { }
 
+  mapMovie (ghibliapiMovies: GhibliapiMovie[]) {
+    return ghibliapiMovies.map(({ movie_banner, ...apiMovie }) => ({ ...apiMovie, banner: movie_banner }))
+  }
+
   async loadMovies (): Promise<MovieProps[]> {
     const movies = []
     while (movies.length < 50) {
       const ghibliapiMovies = (await this.httpClient.get<GhibliapiMovie[]>({ url: 'https://ghibliapi.herokuapp.com/films' }))
-        .map(({ movie_banner, ...apiMovie }) => ({ ...apiMovie, banner: movie_banner }))
-
-      if (ghibliapiMovies.length + movies.length > 50) {
-        const firstSixMovies = ghibliapiMovies.slice(0, 6)
+      const mappedGhibliapiMovies = this.mapMovie(ghibliapiMovies)
+      if (mappedGhibliapiMovies.length + movies.length > 50) {
+        const firstSixMovies = mappedGhibliapiMovies.slice(0, 6)
         movies.push(...firstSixMovies)
       } else {
-        movies.push(...ghibliapiMovies)
+        movies.push(...mappedGhibliapiMovies)
       }
     }
     return movies
